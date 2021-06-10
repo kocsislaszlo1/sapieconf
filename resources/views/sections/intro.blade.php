0cdf329e96ad0c6909db4978f30ff3fb2fab7e1b
unknown <kocsislaszlo@uni.sapientia.ro>
2021-06-10 15:35:03 +0300

    elso

--- resources/views/sections/intro.blade.php
+++ resources/views/sections/intro.blade.php
@@ -1,5 +1,6 @@
 <section id="intro">
   <div class="intro-container wow fadeIn">
+    @if($esmemenyek->esemenynev)
     <h1 class="mb-4 pb-0">{{$esemenyek->esemenynev}}</h1>
     <p class="mb-4 pb-0">{{$esemenyek->tema}}</p>
   
