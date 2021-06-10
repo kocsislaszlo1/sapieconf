dd62e4330be79c8714e5b8eb9c92403ed3be7114
unknown <kocsislaszlo@uni.sapientia.ro>
2021-06-10 16:26:55 +0300

    elso

---
+++ tests/Feature/EloadokRegistTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EloadokRegistTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/regist');
+
+        $response->assertStatus(200);
+    }
+     /**
+     * A valid user can be registered.
+     *
+     * @return void
+     */
+    public function registtest()
+    {
+        $user = ['nev' => 'Nev',
+        'fokozat' => 'Dr',
+        'intezmeny' => 'Sapientia',
+        'eloadascim' => 'cim',
+        'email' => 'valami@gmail.com',
+        'kivonat' => 'kivonat helye ',
+        'szekcio_id' => '1'];
+
+        $response = $this->post('regist',$user);
+        
+
+        $response->assertStatus(302);
+
+        //$this->assertAuthenticated();
+    }
+}
--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -13,10 +13,21 @@ class LoginTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test()
     {
         $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+    public function logintest()
+    {
+        $user = ['email' => 'kocsisaszlo633@gmail.com',
+        'password' => 'valami1234'];
+
+        $response = $this->post('login',$user);
+        
+
+        $response->assertStatus(302);
+
     }
 }
